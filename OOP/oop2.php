5e990f072351debce824ab60e48e429aecf61eae
ranveerrajput <ranveerrajput351@gmail.com>
2020-11-04 06:44:52 +0530

    modified inheritance destructor access modifier

--- OOP/oop2.php
+++ OOP/oop2.php
@@ -19,16 +19,33 @@
 class Fruit{
     private $color;
     private $name;
+
+    //creatung constructor
     function __construct($fruit_name,$fruit_color){
         $this->name=$fruit_name;
         $this->color=$fruit_color;
     }
+
+    //creating method
     public function get_color(){
-        return $this->color .' and  '.$this->name;
+        return $this->color .' and  '.$this->name."<br>";
+        
     }
+
+    //creatig destructor
+    // function __destruct()
+    // {
+    //     echo $this->name.' has been destroyed';
+    //     echo "<br>";
+    // }
 
     
 }
+
+$Mango =new Fruit('mango','yellow');
+echo $Mango->get_color();
+
+//$Mango->color='green';                Cannot perform this since the properties tht is variables are private not public
 
 // //creating an object name as Apple
 // $Apple =new Fruit('Apple');
@@ -37,7 +54,7 @@ class Fruit{
 // $Apple->color='red';
 // echo $Apple->get_color();
 
-class apple extends Fruit{
+class apple extends Fruit{      //inheritance    creating a chlid of the parent class Fruit->apple  apple is a child and Fruit is parent
 
     // public function get_color(){
     //     return $this->color;
@@ -57,6 +74,7 @@ echo "<br>";
  
 $BANANA = new banana('banana','yellow');
 echo $BANANA->get_color();
+echo "<br>";
 
 //$color='red';
 
