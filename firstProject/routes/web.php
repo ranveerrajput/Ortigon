3648b903a0eec2b893b06dfa4320b862ea3ff535
ranveerrajput <ranveerrajput351@gmail.com>
2020-12-03 21:09:07 +0530

    use view and routes

---
+++ firstProject/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+//use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('login',function(){
+    return view('auth/login');
+}); 
+
+Route::post('login',function(Request $request){
+    //echo $request->email . "<br>" . $request->password;
+  
+    $email = $request->email;
+    $password = $request->password;
+    
+
+    return view('user',compact('email', 'password'));
+   
+});
